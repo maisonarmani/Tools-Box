d8f75900677a3dd3e951414ef51ae1114800508a
Maison Armani <masonamrani38@gmail.com>
2017-06-22 11:20:09 +0100

    Staff replacement request and staff requisition form update

--- tools_box/_hr/doctype/staff_replacement_request_form/staff_replacement_request_form.py
+++ tools_box/_hr/doctype/staff_replacement_request_form/staff_replacement_request_form.py
@@ -8,15 +8,16 @@ from frappe.model.document import Document
 
 class StaffReplacementRequestForm(Document):
 	def validate(self):
-		status = str(self.status)
-		if status is not "Open":
+		st = str(self.status)
+		if st in ["Approved", "Rejected"]:
 			if [self.modified_by] not in self.get_approvers():
 				frappe.throw("Only specified approver can approve or reject this document")
 			self.docstatus = 1
 
-		if str(self.supervisor) is str(self.staff_to_be_replaced):
-			frappe.throw("Sorry, Supervisor can not replace him/herself.")
-
+	def get_approvers(self):
+		return frappe.db.sql("""
+					select u.name from tabUser u, `tabHas Role` r where (u.name = r.parent) and r.role = 'Directors' and 
+					u.enabled = 1""", as_list=1)
 	def get_approvers(self):
 		return frappe.db.sql("""
 				select u.name from tabUser u, `tabHas Role` r where (u.name = r.parent) and r.role = 'Directors' and 
