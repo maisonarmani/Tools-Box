b52860bae1bbcd2affa6bac9c93ea6270594ff32
Maison Armani <masonamrani38@gmail.com>
2017-12-03 08:14:56 +0100

    Sales Journey Plan Update

--- tools_box/_selling/doctype/sales_journey_plan/sales_journey_plan.js
+++ tools_box/_selling/doctype/sales_journey_plan/sales_journey_plan.js
@@ -7,7 +7,7 @@ var DMAP = {
     "Wednesday": 2,
     "Thursday": 3,
     "Friday": 4,
-    "Saturday": 5,
+    "Saturday": 5
 };
 
 frappe.ui.form.on('Sales Journey Plan', {
@@ -19,59 +19,68 @@ frappe.ui.form.on('Sales Journey Plan', {
         if (frm.doc.from != undefined) {
             frappe.model.set_value(doctype, docname, "to", frappe.datetime.add_days(frm.doc.from, 6))
         }
-
-        setDOTW(frm)
+        setDOTW(frm);
     }
-
-
 });
 
 
 var opts = {
-    // Set current date value of item from
+    existing_outlets_add: function (frm, doctype, docname) {
+        setPref(frm, doctype, docname)
+    },
     new_outlets_add: function (frm, doctype, docname) {
-        var items = frappe.model.get_list(doctype);
-        if (items.length > 1) {
-            var last = items[items.length - 2]; // last is previous item
-            frappe.model.set_value(doctype, docname, "date", last.date);
-            frappe.model.set_value(doctype, docname, "day_of_the_week", last.day_of_the_week);
-            frappe.model.set_value(doctype, docname, "territory", last.territory);
-        }
-
-        calcTotal(frm, frm.doctype, frm.docname)
-        setDOTW(frm,doctype,docname)
-
+        setPref(frm, doctype, docname)
+    },
+    existing_outlets_remove: function (frm) {
+        sumRoutes(frm, frm.doctype, frm.docname)
     },
     new_outlets_remove: function (frm) {
-        calcTotal(frm, frm.doctype, frm.docname)
+        sumRoutes(frm, frm.doctype, frm.docname)
     },
     day_of_the_week: function (frm, doctype, docname) {
-        setDOTW(frm,doctype,docname)
+        setDOTW(frm, doctype, docname)
     }
 }
 
-frappe.ui.form.on('Sales Journey Plan New Item',opts);
+frappe.ui.form.on('Sales Journey Plan New Item', opts);
 frappe.ui.form.on('Sales Journey Plan Existing Item', opts);
 
-
-var setDOTW = function (frm, doctype, docname) {
-    if (doctype == undefined){
-        frm.doc.existing_outlets.forEach(function(_){
-            setDOTW(frm, _.doctype, _.name)
-        });
-
-        frm.doc.new_outlets.forEach(function(_){
-            setDOTW(frm, _.doctype, _.name)
-        })
+var setPref = function (frm, doctype, docname) {
+    var items = frappe.model.get_list(doctype);
+    if (items.length > 1) {
+        var last = items[items.length - 2]; // last is previous item
+        frappe.model.set_value(doctype, docname, "date", last.date);
+        frappe.model.set_value(doctype, docname, "day_of_the_week", last.day_of_the_week);
+        frappe.model.set_value(doctype, docname, "territory", last.territory);
     }
 
-    var dotw = frappe.model.get_value(doctype, docname, "day_of_the_week")
-    frappe.model.set_value(doctype, docname, "date",
-        frappe.datetime.add_days(frm.doc.from, DMAP[dotw]))
+    sumRoutes(frm, frm.doctype, frm.docname);
+    setDOTW(frm, doctype, docname)
+}
+
+var setDOTW = function (frm, doctype, docname) {
+    var dotw = frappe.model.get_value(doctype, docname, "day_of_the_week");
+    var addition = frappe.datetime.add_days(frm.doc.from, DMAP[dotw]);
+    frappe.model.set_value(doctype, docname, "date",addition);
+
+    if (doctype == undefined) {
+        if (frm.doc.existing_outlets) {
+            frm.doc.existing_outlets.forEach(function (_) {
+                setDOTW(frm, _.doctype, _.name)
+            });
+        }
+
+        if (frm.doc.new_outlets) {
+            frm.doc.new_outlets.forEach(function (_) {
+                setDOTW(frm, _.doctype, _.name)
+            })
+        }
+    }
 }
 
 
-var calcTotal = function(frm, doct, docn) {
+var sumRoutes = function (frm, doct, docn) {
     frappe.model.set_value(doct, docn, "total_routes",
-        frm.doc.existing_outlets.length + frm.doc.new_outlets.length)
+        frm.doc.existing_outlets ? frm.doc.existing_outlets.length : 0  +
+        frm.doc.new_outlets ? frm.doc.new_outlets.length : 0 )
 }
--- tools_box/_stock/doctype/stationaries_request/test_stationaries_request.py
+++ tools_box/_stock/doctype/stationaries_request/test_stationaries_request.py
@@ -6,5 +6,20 @@ from __future__ import unicode_literals
 import frappe
 import unittest
 
+
 class TestStationariesRequest(unittest.TestCase):
-	pass
+    pass
+
+
+def manufactured():
+    d = frappe.db.sql(
+        "SELECT i.name,i.item_name, i.stock_uom ,SUM(po.produced_qty) from `tabItem` i RIGHT OUTER JOIN `tabProduction Order` po "
+        "ON(i.name = po.production_item) WHERE i.is_purchase_item = 0 GROUP BY i.name")
+    print d
+
+
+def issued():
+    d = frappe.db.sql("SELECT i.name,i.item_name, i.stock_uom ,SUM(sed.qty) from `tabItem` i INNER JOIN "
+                      "`tabStock Entry` se RIGHT OUTER JOIN `tabStock Entry Detail` sed ON((i.name = sed.item_code) and (sed.parent = se.name) ) "
+                      "WHERE se.purpose = 'Material Issue' AND i.is_purchase_item = 0 GROUP BY i.name")
+    print d
