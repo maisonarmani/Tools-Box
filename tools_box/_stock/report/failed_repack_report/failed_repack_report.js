8846fbfc2d77e5779efd12eb66af0db207800f8e
Mason Armani <masonarmani38@gmail.com>
2018-05-10 14:17:32 +0100

    Update of failed repack report

--- tools_box/_stock/report/failed_repack_report/failed_repack_report.js
+++ tools_box/_stock/report/failed_repack_report/failed_repack_report.js
@@ -3,7 +3,17 @@
 /* eslint-disable */
 
 frappe.query_reports["Failed Repack Report"] = {
-	"filters": [
-
-	]
+	"filters": [{
+        "fieldname": "from",
+        "label": __("From"),
+        "fieldtype": "Date",
+        "default": frappe.datetime.year_start(),
+        "reqd": 1
+    },{
+        "fieldname": "to",
+        "label": __("To"),
+        "fieldtype": "Date",
+        "default": frappe.datetime.now_datetime(),
+        "reqd": 1
+    }]
 }
